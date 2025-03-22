1dea569e3b67b0132045c3d7a34bae29847cacd7
quyendeptrai <nguyenquangq111@gmail.com>
2025-03-22 15:38:47 +0700

    fix/show-products-admin

--- src/pages/products/show.tsx
+++ src/pages/products/show.tsx
@@ -14,8 +14,16 @@ export const ProductsShow = () => {
     <Show isLoading={isLoading}>
       <Title level={5}>{"ID"}</Title>
       <TextField value={record?.id} />
-      <Title level={5}>{"Title"}</Title>
+      <Title level={5}>{"Tên sản phẩm"}</Title>
       <TextField value={record?.title} />
+      <Title level={5}>{"Giá sản phẩm"}</Title>
+      <TextField value={record?.price} />
+      <Title level={5}>{"Danh mục"}</Title>
+      <TextField value={record?.categories} />
+      <Title level={5}>{"Tình trạng"}</Title>
+      <TextField value={record?.status} />
+      <Title level={5}>{"Mô tả"}</Title>
+      <TextField value={record?.description} />
     </Show>
   );
 };
